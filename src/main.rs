b6a4e4a8e664cfc60f93d858f3e76ab06739c5ea
Mike Liao <mike.hm.liao@gmail.com>
2024-09-05 14:23:56 +0800

    login and cookies

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::net::SocketAddr;
+
+use axum::{extract::{Path, Query}, middleware, response::{Html, IntoResponse, Response}, routing::{get, get_service}, Router};
+use serde::Deserialize;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+
+pub use self::error::{Error, Result};
+
+mod error;
+mod web;
+
+#[tokio::main]
+async fn main() {
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("Listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    println!();
+    res
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+// e.g., `/hello?name=Mike`
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
---
+++ tests/quick_dev.rs
@@ -0,0 +1,23 @@
+use anyhow::Result;
+use serde_json::json;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+    let hc = httpc_test::new_client("http://127.0.0.1:8000")?;
+
+    hc.do_get("/hello2/Mike").await?.print().await?;
+
+    // get the source code of the main.rs file
+    // hc.do_get("/src/main.rs").await?.print().await?;
+
+    let req_login = hc.do_post(
+        "/api/login",
+        json!( {
+            "username": "demo1",
+            "pwd": "welcome"
+        }),
+    );
+    req_login.await?.print().await?;
+
+    Ok(())
+}
